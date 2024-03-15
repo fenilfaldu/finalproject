22bf699ae898eac8be98a177f513fe4773099118
fenilfaldu1 <65482495+fenilfaldu@users.noreply.github.com>
2024-03-15 10:08:44 -0400

    eigth

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,7 +4,7 @@
 sudo pkill -f runserver
 
 # Change directory to the Django project root
-cd /home/ubuntu/finalproject
+cd /home/ubuntu/Django-Training/
 
 # Install the AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
